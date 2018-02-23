28a75d989f002dbb3b75f505bf5b8c8de55a2053
Daniel Testa <dantesta@Daniels-MacBook-Pro.local>
2018-02-23 12:54:49 -0500

    js start

---
+++ assets/js/game.js
@@ -0,0 +1,31 @@
+$( document ).ready(function() {
+
+
+    var yourCharacter;
+    var defender;
+    var characterSelected = false;
+    var defenderSelected = true;
+    var yourCharacterPower;
+    var yourCharacterHp;
+    var defenderPower;
+    var defenderHp;
+    var attack;
+    var attackPlus;
+
+
+function startGame() {
+
+	 reset();
+
+
+
+
+};
+
+function reset () {
+           
+            }
+    
+startGame(); 
+
+});
